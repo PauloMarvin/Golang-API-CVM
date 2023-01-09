7f7acda53f21cc05ebe0d812d8ccf3200160028c
Francisco Prata Cavalcante Neto <francisco.prata@engdb.com.br>
2023-01-09 08:49:58 -0300

    implementing logs for requests

--- cmd/golang-API-kafka/main.go
+++ cmd/golang-API-kafka/main.go
@@ -7,20 +7,17 @@ import (
 )
 
 func main() {
-	fmt.Printf("GO API STARTING")
+	fmt.Print("GO API STARTING")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			fmt.Fprintln(w, "Hello world")
-		}
-	})
-	http.HandleFunc("/now", func(w http.ResponseWriter, r *http.Request) {
-		// Obter horario atual
-		currentTime := time.Now()
-		// Ajustar o horario para o fuso horário de Brasilia
-		brasiliaTime := currentTime.In(time.FixedZone("Brasília", -3*60*60))
-		// Retornar o horario no formato brasileiro
-		fmt.Fprint(w, brasiliaTime.Format("2006-01-02 15:04:05"))
+		fmt.Printf("\n", r)
+		fmt.Fprintf(w, "Hello, World!")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("\n", r)
+		t := time.Now().Format(time.RFC1123)
+		fmt.Fprintf(w, t)
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
